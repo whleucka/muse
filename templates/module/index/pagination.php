c7966fd48cba57089d48915f6064d49f30cc7eb9
William Hleucka <william.hleucka@gmail.com>
2024-04-19 14:20:17 -0600

    wip(admin): polish pagination links

--- templates/module/index/pagination.php
+++ templates/module/index/pagination.php
@@ -2,54 +2,49 @@
 <div class="d-flex">
 	<nav aria-label="Page navigation" hx-boost="true" hx-target="#table" hx-select="#table" hx-swap="outerHTML">
 		<?php if ($total_pages > 1): ?>
-		<ul class="pagination pagination">
-			<li class="page-item <?=$current_page === 1 ? 'disabled' : ''?>"><a id="page-prev" class="page-link" href="?page=<?=$current_page-1?>">&#706;</a></li>
+			<ul class="pagination pagination">
+				<li class="page-item <?=$current_page === 1 ? 'disabled' : ''?>"><a id="page-prev" class="page-link" href="?page=<?=$current_page-1?>">&#706;</a></li>
 
-			<?php if ($current_page < 6): ?>
+				<?php if ($current_page < 6): ?>
 
-				<?php for($i = 1; $i < 6; $i++): ?>
-					<?php if ($i < $total_pages + 1) : ?>
-					<li class="page-item pagination-link <?=$i === $current_page ? 'active' : ''?>" aria-current="page"><a class="page-link" href="?page=<?=$i?>"><?=$i?></a></li>
-					<?php endif ?>
-				<?php endfor ?>
+					<?php for($i = 1; $i < 6; $i++): ?>
+						<?php if ($i < $total_pages + 1) : ?>
+							<li class="page-item pagination-link <?=$i === $current_page ? 'active' : ''?>" aria-current="page"><a class="page-link" href="?page=<?=$i?>"><?=$i?></a></li>
+						<?php endif ?>
+					<?php endfor ?>
 
-				<?php if ($total_pages > 5): ?>
-				<li class="page-item pagination-link disabled" aria-current="page"><a class="page-link" href="#">...</a></li>
-				<li class="page-item pagination-link" aria-current="page"><a class="page-link" href="?page=<?=$total_pages?>"><?=$total_pages?></a></li>
-				<?php endif ?>
-
-			<?php elseif ($total_pages > 5 && $current_page > 5): ?>
-
-				<?php if ($current_page === 1): ?>
-				<li class="page-item pagination-link active" aria-current="page"><a class="page-link" href="?page=1">1</a></li>
-				<?php endif ?>
-
-				<?php if ($current_page > 1): ?>
-				<li class="page-item pagination-link" aria-current="page"><a class="page-link" href="?page=1">1</a></li>
-				<li class="page-item pagination-link disabled" aria-current="page"><a class="page-link" href="#">...</a></li>
-				<?php endif ?>
-
-				<?php for($i = $current_page - $side_links; $i < $current_page + $side_links + 1; $i++): ?>
-					<?php if ($i > 1 && $i < $total_pages) : ?>
-					<li class="page-item pagination-link <?=$i === $current_page ? 'active' : ''?>" aria-current="page"><a class="page-link" href="?page=<?=$i?>"><?=$i?></a></li>
-					<?php endif ?>
-				<?php endfor ?>
-
-				<?php if ($current_page === $total_pages): ?>
-				<li class="page-item pagination-link active" aria-current="page"><a class="page-link" href="?page=<?=$total_pages?>"><?=$total_pages?></a></li>
-				<?php endif ?>
-
-				<?php if ($current_page < $total_pages): ?>
-					<?php if ($current_page != $total_pages - 1): ?>
 					<li class="page-item pagination-link disabled" aria-current="page"><a class="page-link" href="#">...</a></li>
-					<?php endif ?>
 					<li class="page-item pagination-link" aria-current="page"><a class="page-link" href="?page=<?=$total_pages?>"><?=$total_pages?></a></li>
+
+				<?php elseif ($total_pages > 5 && $current_page > 5 && $current_page < $total_pages - 4): ?>
+
+					<li class="page-item pagination-link" aria-current="page"><a class="page-link" href="?page=1">1</a></li>
+					<li class="page-item pagination-link disabled" aria-current="page"><a class="page-link" href="#">...</a></li>
+
+					<?php for($i = $current_page - $side_links; $i < $current_page + $side_links + 1; $i++): ?>
+						<?php if ($i > 1 && $i < $total_pages) : ?>
+							<li class="page-item pagination-link <?=$i === $current_page ? 'active' : ''?>" aria-current="page"><a class="page-link" href="?page=<?=$i?>"><?=$i?></a></li>
+						<?php endif ?>
+					<?php endfor ?>
+
+					<li class="page-item pagination-link disabled" aria-current="page"><a class="page-link" href="#">...</a></li>
+					<li class="page-item pagination-link" aria-current="page"><a class="page-link" href="?page=<?=$total_pages?>"><?=$total_pages?></a></li>
+
+				<?php else: ?>
+
+					<li class="page-item pagination-link" aria-current="page"><a class="page-link" href="?page=1">1</a></li>
+					<li class="page-item pagination-link disabled" aria-current="page"><a class="page-link" href="#">...</a></li>
+
+					<?php for($i = $total_pages - 4; $i < $total_pages + 1; $i++): ?>
+						<?php if ($i < $total_pages + 1) : ?>
+							<li class="page-item pagination-link <?=$i === $current_page ? 'active' : ''?>" aria-current="page"><a class="page-link" href="?page=<?=$i?>"><?=$i?></a></li>
+						<?php endif ?>
+					<?php endfor ?>
+
 				<?php endif ?>
 
-			<?php endif ?>
-
-			<li class="page-item <?=$current_page === $total_pages ? 'disabled' : ''?>"><a id="page-next" class="page-link" href="?page=<?=$current_page+1?>">&#707;</a></li>
-		</ul>
+				<li class="page-item <?=$current_page === $total_pages ? 'disabled' : ''?>"><a id="page-next" class="page-link" href="?page=<?=$current_page+1?>">&#707;</a></li>
+			</ul>
 		<?php endif ?>
 	</nav>
 	<div class="flex-grow-1"></div>
