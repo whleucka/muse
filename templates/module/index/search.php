f0a4ca520926c395e768250154c048891be96fc7
William Hleucka <william.hleucka@gmail.com>
2024-04-23 16:39:55 -0600

    fix: arrow.svg

--- templates/module/index/search.php
+++ templates/module/index/search.php
@@ -1,5 +1,7 @@
 <?php if($show): ?>
 <div id="filter-search">
-	<input hx-get="?term" hx-swap="outerHTML" hx-target="#table" hx-select="#table" hx-trigger="input changed delay:500ms, search" name="term" value="<?=$term?>" type="search" class="form-control" placeholder="...">
+	<input hx-get="?term" hx-swap="outerHTML" hx-target="#table" hx-select="#table"
+		hx-trigger="input changed delay:500ms, search" name="term" value="<?=$term?>" type="search" class="form-control"
+		placeholder="Search">
 </div>
 <?php endif ?>
