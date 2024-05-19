e43e06fdc7361947db0092915637673ca3c6bcb4
William Hleucka <william.hleucka@gmail.com>
2024-05-19 11:44:12 -0600

    fix: update hx-indicator

--- templates/layout/navbar.php
+++ templates/layout/navbar.php
@@ -1,4 +1,4 @@
-<nav id="navbar" class="navbar navbar-expand-sm bg-light" hx-indicator="#page-request">
+<nav id="navbar" class="navbar navbar-expand-sm bg-light">
   <div class="container-fluid">
     <a class="navbar-brand d-flex align-items-center" hx-get="/playlist" hx-target="#main" hx-select="#main" hx-swap="outerHTML"><img src="/img/logo.png" width="32" height="32" /></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#top-nav"
