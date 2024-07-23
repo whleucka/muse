566db0e99cdf20c1b7217af5cd838297e9b06d7e
William Hleucka <william.hleucka@gmail.com>
2024-07-23 14:31:45 +0000

    fix: refactor podcast input

--- templates/muse/podcast/index.php
+++ templates/muse/podcast/index.php
@@ -7,19 +7,20 @@
 		</span>
 	</h3>
 	<div id="search-input" class="mt-1">
-		<form onkeydown="return event.key != 'Enter';">
+		<form hx-post="/search/podcast"
+			hx-target="#results"
+			hx-indicator="#podcasts .htmx-indicator">
 			<?=$csrf()?>
-			<input id="input"
-				hx-post="/search/podcast"
-				hx-trigger="load, input changed delay:500ms, search"
-				hx-sync="closest form:abort"
-				hx-target="#results"
-				hx-indicator=".htmx-indicator"
-				class="form-control"
-				type="search"
-				name="term"
-				value=""
-				placeholder="What do you want to listen to?" />
+			<div class="input-group">
+				<input id="input"
+					hx-sync="closest form:abort"
+					class="form-control"
+					type="search"
+					name="term"
+					value=""
+					placeholder="What do you want to listen to?" />
+				<button id="search-submit" type="submit" class="btn btn-app" hx-sync="closest form:abort">OK</button>
+			</div>
 		</form>
 	</div>
 	<div id="results" class="mt-3">
